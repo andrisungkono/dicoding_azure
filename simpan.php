6a06ad3fb643a478b9490e67b52d5ee42521b102
andrisungkono <aikono@batan.go.id>
2019-06-03 14:40:26 +0700

    Update simpan.php

--- simpan.php
+++ simpan.php
@@ -5,19 +5,21 @@
         try {
             $nm_resep = $_POST['nm_resep'];
             $isi_resep = $_POST['isi_resep'];
-	    $id_resep = uniqid();
+			$id_resep = uniqid();
+			$create_date = date("Y-m-d");
             // Insert data
-            $sql_insert = "INSERT INTO dbo.resep (id_resep, nm_resep, isi_resep) 
-                        VALUES (?,?,?)";
+            $sql_insert = "INSERT INTO dbo.resep (id_resep, nm_resep, isi_resep, create_date) 
+                        VALUES (?,?,?,?)";
             $stmt = $conn->prepare($sql_insert);
             $stmt->bindValue(1, $id_resep);
             $stmt->bindValue(2, $nm_resep);
             $stmt->bindValue(3, $isi_resep);
+			$stmt->bindValue(4, $create_date);
             $stmt->execute();
         } catch(Exception $e) {
             echo "Failed: " . $e;
         }
 
-       echo "<meta http-equiv='refresh' content='0; url=https://sub01.azurewebsites.net'>";
+        echo "<meta http-equiv='refresh' content='0; url=https://sub01.azurewebsites.net'>";
     } 
  ?>
