fbbfb67ae45857381c3b888d1884b388785f5606
andrisungkono <aikono@batan.go.id>
2019-06-03 13:12:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
  	h2 { font-size: 1.75em; }
  	h3 { font-size: 1.2em; }
  	table { margin-top: 0.75em; }
- 	th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
+ 	th { font-size: 1.2em; text-align: left; border: 1; padding-left: 0; }
  	td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
  </style>
 </head>
