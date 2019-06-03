b2523868a5ed19aa4fb90fc05f9cdbf008d0289e
andrisungkono <aikono@batan.go.id>
2019-06-03 13:17:18 +0700

    Add files via upload

---
+++ tambah.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Tambah Resep</title>
+</head>
+
+<body>
+ <form method="post" action="simpan.php" enctype="multipart/form-data" >
+       Nama Resep  <input type="text" name="name" id="name"/></br></br>
+       Isi Resep <input type="text" name="job" id="job"/></br></br>
+       <input type="submit" name="submit" value="Submit" />
+       <input type="submit" name="load_data" value="Load Data" />
+ </form>
+</body>
+</html>
