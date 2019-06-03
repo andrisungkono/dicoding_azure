f661874b03f788c0c379b73f165a83faecb54b2e
andrisungkono <aikono@batan.go.id>
2019-06-03 11:22:37 +0700

    Update cobakoneksi.php

--- cobakoneksi.php
+++ cobakoneksi.php
@@ -1,44 +1,16 @@
-<html>
- <head>
- <Title>Registration Form</Title>
- <style type="text/css">
- 	body { background-color: #fff; border-top: solid 10px #000;
- 	    color: #333; font-size: .85em; margin: 20; padding: 20;
- 	    font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
- 	}
- 	h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
- 	h1 { font-size: 2em; }
- 	h2 { font-size: 1.75em; }
- 	h3 { font-size: 1.2em; }
- 	table { margin-top: 0.75em; }
- 	th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
- 	td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
- </style>
- </head>
- <body>
- <h1>Register here!</h1>
- <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
- <form method="post" action="index.php" enctype="multipart/form-data" >
-       Name  <input type="text" name="name" id="name"/></br></br>
-       Email <input type="text" name="email" id="email"/></br></br>
-       Job <input type="text" name="job" id="job"/></br></br>
-       <input type="submit" name="submit" value="Submit" />
-       <input type="submit" name="load_data" value="Load Data" />
- </form>
- <?php
-	$host = "dicodingsubmissonsqlserv.database.windows.net,1433";
-    $user = "yangpunyaserver";
-    $pass = "p4w0n_m4g3n4";
-    $db = "db_resep";
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:dicodingsubmissonsqlserv.database.windows.net,1433; Database = db_resep", "yangpunyaserver", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
-    try {
-        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
-        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    } catch(Exception $e) {
-        echo "Failed: " . $e;
-    }
-
-  
- ?>
- </body>
- </html>
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "yangpunyaserver@dicodingsubmissonsqlserv", "pwd" => "p4w0n_m4g3n4", "Database" => "db_resep", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:dicodingsubmissonsqlserv.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
